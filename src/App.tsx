9a2339be8b432d79f271bfca647f6bdae7135267
Roberson Costa Dresch <robersoncosta96@gmail.com>
2023-10-11 17:14:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, StatusBar, useColorScheme, View } from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+
+      <View />
+    </SafeAreaView>
+  );
+}
+
+export default App;
